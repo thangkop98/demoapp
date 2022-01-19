47fc7db241707497e212c06e87b3c5f5f025d6e1
thangkop98 <hoangvuthang270898@gmail.com>
2022-01-19 17:00:05 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demoapp';
+
+  show = true;
+
+  toggle()
+  {
+    
+    this.show = !this.show;
+  }
+}
